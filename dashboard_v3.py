683b1a0df17ee7e54bf5cb6d53969723b059ed43
Pierre Schifflers <schiffpierre@gmail.com>
2020-09-11 22:01:25 +0200

    Adding requirements file and Procfile. Removing unused modules in dashboard file.

--- dashboard_v3.py
+++ dashboard_v3.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import requests
 import json
